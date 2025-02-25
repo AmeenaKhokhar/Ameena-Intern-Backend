b1e3af91d80b3c5267a42603d34f2f8d10236720
AmeenaKhokhar <infoamkhok@gmai.com>
2025-02-24 20:58:14 -0500

    Add type definitions for Todo's, mutation and query

--- src/graphql/typeDefinitions.ts
+++ src/graphql/typeDefinitions.ts
@@ -1,18 +1,45 @@
 export const typeDefs = /* GraphQL */ `
-  input CreateSomethingInput {
-    name: String!
+  scalar DateTime
+
+  # Input for creating a Todo, when creating, should only include string and completed status
+  input CreateTodoInput {
+    title: String!
+    completed: Boolean!
   }
 
-  type Something {
+  # Input for updating a Todo, to update a todo u need id, string or boolean update status
+  input UpdateTodoInput {
     id: ID!
-    name: String!
+    title: String!
+    completed: Boolean!
   }
 
+  # Input for deleting a Todo (need todo ID)
+  input DeleteTodoInput {
+    id: ID!
+  }
+
+  # Type definition of a todo, should have id, title, completed, createdAt and nullable updatedAt 
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+    createdAt: DateTime!
+    updatedAt: DateTime
+  }
+
+  # CUD operations, the inputs are necessary and return the specified TODO to CUD
   type Mutation {
-    createSomething(input: CreateSomethingInput!): Something!
+    createTodo(input: CreateTodoInput!): Todo!
+    updateTodo(input: UpdateTodoInput!): Todo!
+    deleteTodo(input: DeleteTodoInput!): Todo!
   }
 
+  # READ operations to get infos, it always returns an array of TODO's OR EMPTY. For getting todo, if there's no, returns null.
   type Query {
-    hello: String
+    getTodos: [Todo!]!
+    getIncompleteTodos: [Todo!]!
+    getCompletedTodos: [Todo!]!
+    getTodoById(id: ID!): Todo
   }
 `;
